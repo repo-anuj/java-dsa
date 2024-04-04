4032177e351be270b6db27bb8035b0c5dfe82a52
repo-anuj <anujd4348@gmail.com>
2024-04-04 16:09:57 +0530

    print the greeting in switch program

--- conditional-statement/src/print_the_greeting.java
+++ conditional-statement/src/print_the_greeting.java
@@ -6,14 +6,32 @@ public class print_the_greeting {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
 
-        if (x == 1) {
-            System.out.println("Hello");
-        } else if (x == 2) {
-            System.out.println("namaste");
-        } else if (x == 3) {
-            System.out.println("bonjour");
-        } else {
-            System.out.println("invalid input");
+        // if (x == 1) {
+        // System.out.println("Hello");
+        // } else if (x == 2) {
+        // System.out.println("namaste");
+        // } else if (x == 3) {
+        // System.out.println("bonjour");
+        // } else {
+        // System.out.println("invalid input");
+        // }
+
+        switch (x) {
+            case 1:
+                System.out.println("Hello");
+                break;
+
+            case 2:
+                System.out.println("namaste");
+                break;
+
+            case 3:
+                System.out.println("bonjour");
+                break;
+
+            default:
+                System.out.println("invalid input");
+                break;
         }
     }
 }
